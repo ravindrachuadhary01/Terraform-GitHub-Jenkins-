9f8dfd0eee7686fd01ecb6a5f4bf95c03f3b1b84
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-27 18:03:11 +0530

    update

--- main.tf
+++ main.tf
@@ -1,26 +1,37 @@
-resource "aws_instance" "ec2" {
-  count = 2
-
-  ami           = "ami-0f5ee92e2d63afc18"
+resource "aws_launch_template" "lt" {
+  name_prefix   = "app-template"
+  image_id      = "ami-0f5ee92e2d63afc18"
   instance_type = "t3.micro"
 
-  subnet_id = element([
+  vpc_security_group_ids = [aws_security_group.sg.id]
+   update_default_version = true
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum install -y nginx
+    systemctl start nginx
+    systemctl enable nginx
+    echo "Hello from ASG $(hostname)" > /usr/share/nginx/html/index.html
+  EOF
+  )
+}
+
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity = 2
+  max_size         = 3
+  min_size         = 1
+
+  vpc_zone_identifier = [
     aws_subnet.public_1.id,
     aws_subnet.public_2.id
-  ], count.index)
+  ]
 
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  target_group_arns = [aws_lb_target_group.tg.arn]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-              echo "Hello from EC2 $(hostname)" > /usr/share/nginx/html/index.html
-              EOF
+  health_check_type = "ELB"
 
-  tags = {
-    Name = "App-Server-${count.index}"
+  launch_template {
+    id      = aws_launch_template.lt.id
+    version = "$Latest"
   }
 }
