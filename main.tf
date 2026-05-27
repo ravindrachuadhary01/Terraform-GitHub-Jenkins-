6c7e1e995194b7f03bb2ed6d9b15d5b371eca15f
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-27 17:34:08 +0530

    update

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-
 resource "aws_instance" "ec2" {
-  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux (ap-south-1)
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public.id
+
+  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "Terraform-Demo"
+    Name = "Terraform-EC2"
   }
 }
