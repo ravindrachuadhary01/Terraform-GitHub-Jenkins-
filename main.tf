ad28c1022e4af18c06cc048fa177ceba0fd15f02
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-27 17:28:09 +0530

    update

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_security_group" "sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_instance" "ec2" {
   ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux (ap-south-1)
