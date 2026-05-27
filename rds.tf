9fa55e6b661df352eaf18d8f816dc8c59ad491c9
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-27 19:05:32 +0530

    update

---
+++ rds.tf
@@ -0,0 +1,56 @@
+# DB Subnet Group
+resource "aws_db_subnet_group" "db_subnet" {
+  name = "app-db-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.private_db_1.id,
+    aws_subnet.private_db_2.id
+  ]
+
+  tags = {
+    Name = "DBSubnetGroup"
+  }
+}
+
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name   = "rds-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS Instance
+resource "aws_db_instance" "mysql" {
+  identifier             = "app-mysql-db"
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+
+  username               = "admin"
+  password               = "Admin12345"
+
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  multi_az               = false
+
+  tags = {
+    Name = "App-RDS"
+  }
+}
