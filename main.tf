fd15f7a26060f6d69c651d005c6eb5c5911cb883
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-27 17:40:18 +0530

    update

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 resource "aws_instance" "ec2" {
+  count = 2
+
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t3.micro"
 
-  subnet_id              = aws_subnet.public_1.id
+  subnet_id = element([
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ], count.index)
+
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = "App-Server-${count.index}"
   }
 }
+user_data = <<-EOF
+#!/bin/bash
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
+echo "Hello from EC2 $(hostname)" > /usr/share/nginx/html/index.html
+EOF
