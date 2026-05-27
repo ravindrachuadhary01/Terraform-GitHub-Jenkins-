e8a967b8deafc5b9b03a4a72e9eb3d175ee55ec9
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-28 00:46:06 +0530

    update

--- rds.tf
+++ rds.tf
@@ -1,4 +1,4 @@
-# DB Subnet Group
+# ---------------- DB Subnet Group ----------------
 resource "aws_db_subnet_group" "db_subnet" {
   name = "app-db-subnet-group"
 
@@ -12,41 +12,47 @@ resource "aws_db_subnet_group" "db_subnet" {
   }
 }
 
-# RDS Security Group
+# ---------------- RDS Security Group ----------------
 resource "aws_security_group" "rds_sg" {
   name   = "rds-sg"
   vpc_id = aws_vpc.main.id
 
+  # Allow only EC2 SG to access MySQL
   ingress {
-    from_port = 3306
-    to_port   = 3306
-    protocol  = "tcp"
-
-    # IMPORTANT: allow EC2 security group
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = [aws_security_group.sg.id]
   }
 
+  # Outbound allowed
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "rds-sg"
+  }
 }
 
-# RDS Instance
+# ---------------- RDS Instance ----------------
 resource "aws_db_instance" "mysql" {
-  identifier         = "app-mysql-db"
-  allocated_storage  = 20
-  storage_type       = "gp2"
+  identifier = "app-mysql-db"
 
-  engine             = "mysql"
-  engine_version     = "8.0"
+  engine         = "mysql"
+  engine_version = "8.0"
+  instance_class = "db.t3.micro"
 
-  instance_class     = "db.t3.micro"
+  allocated_storage = 20
+  storage_type      = "gp2"
 
-  username           = "admin"
-  password           = "Admin12345"
+  username = "admin"
+  password = "Admin12345"
+
+  db_name = "mydb"
 
   publicly_accessible = false
   skip_final_snapshot = true
@@ -58,4 +64,9 @@ resource "aws_db_instance" "mysql" {
   tags = {
     Name = "App-RDS"
   }
+}
+
+# ---------------- OUTPUT (IMPORTANT) ----------------
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.address
 }
