dbffe3b35da82b2e5eba410de0b11494ad6e9604
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-28 10:43:16 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,10 @@ import pymysql
 
 app = Flask(__name__)
 
-DB_HOST = "app-mysql-db.cr04wsu2e9r0.ap-south-1.rds.amazonaws.com"
+DB_HOST = "YOUR-RDS-ENDPOINT"
 DB_USER = "admin"
 DB_PASSWORD = "Admin12345"
+DB_NAME = "mydb"
 
 @app.route("/")
 def home():
@@ -13,13 +14,14 @@ def home():
         connection = pymysql.connect(
             host=DB_HOST,
             user=DB_USER,
-            password=DB_PASSWORD
+            password=DB_PASSWORD,
+            database=DB_NAME
         )
 
-        return "✅ Flask Backend Connected to RDS Successfully!"
+        return "✅ Flask App Connected to RDS Successfully!"
 
     except Exception as e:
-        return f"Database Connection Failed: {e}"
+        return f"❌ Database Connection Failed: {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
