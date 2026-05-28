56a662128b0a10a8ad0417b36c7607b0e0d7e8f4
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-28 14:52:14 +0530

    update

--- rds.tf
+++ rds.tf
@@ -1,17 +1,46 @@
+########################################
+# DB SUBNET GROUP
+########################################
+resource "aws_db_subnet_group" "main" {
+  name = "main-db-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+
+  tags = {
+    Name = "main-db-subnet-group"
+  }
+}
+
+########################################
+# RDS MYSQL INSTANCE
+########################################
 resource "aws_db_instance" "mysql" {
-  identifier         = "mysql-db"
-  engine             = "mysql"
-  engine_version     = "8.0"
-  instance_class     = "db.t3.micro"
+  identifier = "my-rds-db"
 
-  allocated_storage  = 20
+  engine         = "mysql"
+  engine_version = "8.0"
 
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+
+  db_name  = "appdb"
   username = "admin"
   password = "Admin12345"
 
   db_subnet_group_name   = aws_db_subnet_group.main.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   publicly_accessible = false
+
   skip_final_snapshot = true
+  deletion_protection  = false
+
+  backup_retention_period = 0
+
+  tags = {
+    Name = "mysql-rds"
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,45 @@
+########################################
+# VPC
+########################################
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "main-vpc"
   }
 }
 
-resource "aws_subnet" "public_1" {
+########################################
+# INTERNET GATEWAY
+########################################
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+########################################
+# PUBLIC SUBNET
+########################################
+resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-1"
+    Name = "public-subnet"
   }
 }
 
-resource "aws_subnet" "private_1" {
+########################################
+# PRIVATE SUBNET 1
+########################################
+resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-south-1a"
@@ -27,7 +49,10 @@ resource "aws_subnet" "private_1" {
   }
 }
 
-resource "aws_subnet" "private_2" {
+########################################
+# PRIVATE SUBNET 2
+########################################
+resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-south-1b"
@@ -36,32 +61,36 @@ resource "aws_subnet" "private_2" {
     Name = "private-subnet-2"
   }
 }
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
 
+########################################
+# INTERNET ROUTE TABLE
+########################################
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id  = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
   }
 }
 
+########################################
+# ROUTE TABLE ASSOCIATION
+########################################
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_1.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
-resource "aws_security_group" "public_sg" {
-  vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+########################################
+# ALB SECURITY GROUP
+########################################
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 80
@@ -76,12 +105,90 @@ resource "aws_security_group" "public_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-resource "aws_db_subnet_group" "main" {
-  name = "main-db-subnet-group"
 
-  subnet_ids = [
-    aws_subnet.private_1.id,
-    aws_subnet.private_2.id
-  ]
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+########################################
+# PUBLIC EC2 SECURITY GROUP
+########################################
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-sg"
+  }
+}
+
+########################################
+# PRIVATE EC2 SECURITY GROUP
+########################################
+resource "aws_security_group" "private_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "private-sg"
+  }
+}
+
+########################################
+# RDS SECURITY GROUP
+########################################
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.private_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
 }
