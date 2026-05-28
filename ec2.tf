5ce39b62bb28a649883ffb65b6d07d08bf075260
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-28 13:50:16 +0530

    update

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+# Public EC2 Instance
+resource "aws_instance" "public_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  key_name = aws_key_pair.deployer_key.key_name
+
+  tags = {
+    Name = "Public-EC2"
+  }
+}
+
+# Private EC2 Instance
+resource "aws_instance" "private_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private_subnet.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+ key_name = aws_key_pair.deployer_key.key_name
+
+  tags = {
+    Name = "Private-EC2"
+  }
+}
