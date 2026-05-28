3d09769e801db039e7fff51f9886ffecc24796ec
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-28 10:49:59 +0530

    update

--- main.tf
+++ main.tf
@@ -45,30 +45,3 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# RDS Instance (FIXED connectivity settings)
-resource "aws_db_instance" "mysql" {
-  identifier         = "app-mysql-db"
-  engine             = "mysql"
-  engine_version     = "8.0"
-  instance_class     = "db.t3.micro"
-
-  allocated_storage  = 20
-
-  username           = "admin"
-  password           = "Admin12345"
-
-  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-
-  publicly_accessible = false
-
-  skip_final_snapshot = true
-  multi_az            = false
-
-  deletion_protection = false
-
-  tags = {
-    Name = "App-RDS"
-  }
-}
