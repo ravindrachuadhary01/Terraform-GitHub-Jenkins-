0df5ff22b41497ca95c12cc6204d36cd509d1474
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-28 13:31:21 +0530

    update

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "Ubuntu AMI ID"
+  type        = string
+  default     = "ami-0f5ee92e2d63afc18"
+}
+
+variable "key_name" {
+  description = "AWS Key Pair Name"
+  type        = string
+}
--- vpc.tf
+++ vpc.tf
@@ -1,88 +1,54 @@
-
-# ---------------- VPC ----------------
+# VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "3tier-vpc"
+    Name = "Main-VPC"
   }
 }
 
-# ---------------- PUBLIC SUBNETS ----------------
-resource "aws_subnet" "public_1" {
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-1"
+    Name = "Public-Subnet"
   }
 }
 
-resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1b"
-  map_public_ip_on_launch = true
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
 
   tags = {
-    Name = "public-subnet-2"
+    Name = "Private-Subnet"
   }
 }
-
-# ---------------- PRIVATE APP SUBNETS ----------------
-resource "aws_subnet" "private_app_1" {
+# Private Subnet 2
+resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "private-app-1"
-  }
-}
-
-resource "aws_subnet" "private_app_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
   availability_zone = "ap-south-1b"
 
   tags = {
-    Name = "private-app-2"
+    Name = "Private-Subnet-2"
   }
 }
-
-# ---------------- PRIVATE DB SUBNETS ----------------
-resource "aws_subnet" "private_db_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.5.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "private-db-1"
-  }
-}
-
-resource "aws_subnet" "private_db_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.6.0/24"
-  availability_zone = "ap-south-1b"
-
-  tags = {
-    Name = "private-db-2"
-  }
-}
-
-# ---------------- INTERNET GATEWAY ----------------
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main-igw"
+    Name = "Main-IGW"
   }
 }
 
-# ---------------- PUBLIC ROUTE TABLE ----------------
+# Public Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -92,17 +58,12 @@ resource "aws_route_table" "public_rt" {
   }
 
   tags = {
-    Name = "public-rt"
+    Name = "Public-RT"
   }
 }
 
-# ---------------- ASSOCIATE PUBLIC SUBNETS ----------------
-resource "aws_route_table_association" "public_1_assoc" {
-  subnet_id      = aws_subnet.public_1.id
+# Route Table Association for Public Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
-}
 
-resource "aws_route_table_association" "public_2_assoc" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public_rt.id
-}
