3f65cbfac55d2022ffe4278fad6886f18189270c
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-28 14:26:49 +0530

    update

--- sg.tf
+++ sg.tf
@@ -1,18 +1,50 @@
-resource "aws_security_group" "sg" {
-  name        = "web-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.main.id
 
   ingress {
-    description = "SSH"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  vpc_id = aws_vpc.main.id
+
   ingress {
-    description = "HTTP"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
