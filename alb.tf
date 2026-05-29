6673be018763abe33e7b1b367d26385714af7a8b
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-29 08:23:13 +0530

    update

--- alb.tf
+++ alb.tf
@@ -31,7 +31,7 @@ resource "aws_lb_target_group" "tg" {
 #aws_lb_target_group_attachment
 resource "aws_lb_target_group_attachment" "flask_attach" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = i-0da165d90e53709bf
+  target_id        = aws_instance.flask.id
   port             = 5000
 }
 # Listener
