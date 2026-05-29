7c01e93d0d50a90b45019a8f1fde88a05487f238
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-29 14:02:03 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,26 @@
 from flask import Flask
-from flask_cors import CORS
+import pymysql
 
 app = Flask(__name__)
-CORS(app)
+
+conn = pymysql.connect(
+    host="app-mysql-db.cr04wsu2e9r0.ap-south-1.rds.amazonaws.com",
+    user="admin",
+    password="Admin12345",
+    database="mysql",
+    port=3306
+)
 
 @app.route("/")
 def home():
-    return "Flask Backend Running"
+    return "Flask + RDS Connected"
+
+@app.route("/db")
+def db_check():
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1")
+    result = cursor.fetchone()
+    return f"DB Response: {result}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
