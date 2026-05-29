f2e265e43cd6afed523fe2d226209bdac41a68ee
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-29 13:02:17 +0530

    update

--- main.tf
+++ main.tf
@@ -73,13 +73,13 @@ resource "aws_security_group" "backend_sg" {
     from_port       = 5000
     to_port         = 5000
     protocol        = "tcp"
-    security_groups = [aws_security_group.frontend_sg.id]
+    security_groups = [aws_security_group.sg.id]
   }
 ingress {
   from_port       = 22
   to_port         = 22
   protocol        = "tcp"
-  security_groups = [aws_security_group.frontend_sg.id]
+   security_groups = [aws_security_group.frontend_sg.id]
 }
   egress {
     from_port   = 0
