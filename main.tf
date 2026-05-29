00c8909288f78ac0adea2894c5e19a1988f6bb07
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-30 02:22:11 +0530

    update

--- main.tf
+++ main.tf
@@ -1,92 +1,31 @@
-locals {
-  frontend_user_data = <<-EOF
-#!/bin/bash
-set -ex
-exec > /var/log/user-data.log 2>&1
-
-apt update -y
-apt install -y docker.io awscli
-
-systemctl enable docker
-systemctl start docker
-
-sudo usermod -aG docker $USER
-
-# wait for docker
-until systemctl is-active --quiet docker; do
-  sleep 2
-done
-
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 192902842773.dkr.ecr.ap-south-1.amazonaws.com
-
-docker rm -f frontend || true
-
-docker pull 192902842773.dkr.ecr.ap-south-1.amazonaws.com/frontend-repo:latest
-
-docker run -d --restart always \
-  --name frontend \
-  -p 8080:80 \
-  192902842773.dkr.ecr.ap-south-1.amazonaws.com/frontend-repo:latest
-EOF
-
-  backend_user_data = <<-EOF
-#!/bin/bash
-set -ex
-exec > /var/log/user-data.log 2>&1
-
-apt update -y
-apt install -y docker.io awscli
-
-systemctl enable docker
-systemctl start docker
-
-sudo usermod -aG docker $USER
-
-until systemctl is-active --quiet docker; do
-  sleep 2
-done
-
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 192902842773.dkr.ecr.ap-south-1.amazonaws.com
-
-docker rm -f flask-app || true
-
-docker pull 192902842773.dkr.ecr.ap-south-1.amazonaws.com/flask-backend:latest
-
-docker run -d --restart always \
-  --name flask-app \
-  -p 5000:5000 \
-  -e DB_HOST=${aws_db_instance.mysql.address} \
-  -e DB_USER=admin \
-  -e DB_PASS=Admin12345 \
-  -e DB_NAME=appdb \
-  192902842773.dkr.ecr.ap-south-1.amazonaws.com/flask-backend:latest
-EOF
-}
-resource "aws_instance" "ec2" {
-  count         = 2
+resource "aws_instance" "frontend" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t3.micro"
-  key_name      = "three-tier-key"
 
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  subnet_id              = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.frontend_sg.id]
 
-  subnet_id = element([
-    aws_subnet.public_1.id,
-    aws_subnet.private_app_1.id
-  ], count.index)
+  user_data = filebase64("${path.module}/userf-data.sh")
 
-  vpc_security_group_ids = count.index == 0 ? [
-    aws_security_group.frontend_sg.id
-  ] : [
-    aws_security_group.backend_sg.id
-  ]
-
-  associate_public_ip_address = count.index == 0 ? true : false
-
-  user_data = count.index == 0 ? local.frontend_user_data : local.backend_user_data
+  user_data_replace_on_change = true
 
   tags = {
-    Name = count.index == 0 ? "Public-Frontend" : "Private-Backend"
+    Name = "frontend"
+  }
+}
+resource "aws_instance" "backend" {
+  ami           = "ami-03f4878755434977f"
+  instance_type = "t3.micro"
+
+  subnet_id              = aws_subnet.private_app_1.id
+  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+
+  user_data = filebase64("${path.module}/userb-data.sh")
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "backend"
   }
 }
 # -------------------------
