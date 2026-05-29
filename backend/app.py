eeceeac1abe18089e4a6737b966221526aa431ee
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-29 12:45:37 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/data")
+@app.route("/")
 def home():
-    return jsonify({"message": "Backend working via ALB"})
+    return "Flask Backend Running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
