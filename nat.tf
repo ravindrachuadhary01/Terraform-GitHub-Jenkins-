01b46b066d37ae3645611946ba44b76e9fc97eea
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-30 01:13:36 +0530

    update

--- nat.tf
+++ nat.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_subnet_1.id
+  aws_subnet.public_subnet_1.id
 
   tags = {
     Name = "nat-gateway"
