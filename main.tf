ff2dd2d79e9578a972718c1dd636ffba15feda9b
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-29 19:49:00 +0530

    update

--- main.tf
+++ main.tf
@@ -1,40 +1,71 @@
 resource "aws_instance" "ec2" {
-  count = 2
+  count         = 2
   ami           = "ami-03f4878755434977f"
   instance_type = "t3.micro"
-  key_name = "three-tier-key"
+  key_name      = "three-tier-key"
 
-  # First instance -> Public subnet
-  # Second instance -> Private subnet
   subnet_id = element([
     aws_subnet.public_1.id,
     aws_subnet.private_app_1.id
   ], count.index)
 
-  # 🔥 FIX: separate security groups properly
   vpc_security_group_ids = count.index == 0 ? [
     aws_security_group.frontend_sg.id
   ] : [
     aws_security_group.backend_sg.id
   ]
 
-  # Public IP only for public instance
   associate_public_ip_address = count.index == 0 ? true : false
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-              echo "Hello from EC2 $(hostname)" > /var/www/html/index.nginx-debian.html
-              EOF
+  user_data = count.index == 0 ? <<-EOF
+    #!/bin/bash
+    set -e
+
+    apt update -y
+    apt install -y docker.io awscli
+
+    systemctl start docker
+    systemctl enable docker
+
+    aws ecr get-login-password --region ap-south-1 | \
+    docker login --username AWS --password-stdin 192902842773.dkr.ecr.ap-south-1.amazonaws.com
+
+    docker rm -f frontend || true
+
+    docker pull 192902842773.dkr.ecr.ap-south-1.amazonaws.com/frontend-repo:latest
+
+    docker run -d \
+    --name frontend \
+    -p 8080:80 \
+    192902842773.dkr.ecr.ap-south-1.amazonaws.com/frontend-repo:latest
+  EOF
+  : <<-EOF
+    #!/bin/bash
+    set -e
+
+    apt update -y
+    apt install -y docker.io awscli
+
+    systemctl start docker
+    systemctl enable docker
+
+    aws ecr get-login-password --region ap-south-1 | \
+    docker login --username AWS --password-stdin 192902842773.dkr.ecr.ap-south-1.amazonaws.com
+
+    docker rm -f flask-app || true
+
+    docker pull 192902842773.dkr.ecr.ap-south-1.amazonaws.com/flask-backend:latest
+
+    docker run -d \
+    --name flask-app \
+    -p 5000:5000 \
+    192902842773.dkr.ecr.ap-south-1.amazonaws.com/flask-backend:latest
+  EOF
 
   tags = {
-    Name = count.index == 0 ? "Public-Server" : "Private-Server"
+    Name = count.index == 0 ? "Public-Frontend" : "Private-Backend"
   }
 }
-
 # -------------------------
 # FRONTEND SECURITY GROUP
 # -------------------------
@@ -52,7 +83,14 @@ ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   
+}
+ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    
 }
   egress {
     from_port   = 0
