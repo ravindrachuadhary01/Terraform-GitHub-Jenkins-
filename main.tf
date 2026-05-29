514c0f5c5c2b7c8f5b9f025035fd14e2b6bd6990
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-29 11:34:47 +0530

    update

--- main.tf
+++ main.tf
@@ -47,13 +47,13 @@ resource "aws_security_group" "frontend_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+  }
+ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
+}
   egress {
     from_port   = 0
     to_port     = 0
