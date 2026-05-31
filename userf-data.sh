6fd29c59426317596ce01bb819d5ce257e85d09f
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-31 16:38:44 +0530

    update

--- userf-data.sh
+++ userf-data.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
-set -ex
+
+# Logs
 exec > /var/log/user-data.log 2>&1
 
+# Update
 apt update -y
-apt install -y docker.io awscli
 
-systemctl enable docker
+# Install Docker
+apt install docker.io -y
+
 systemctl start docker
+systemctl enable docker
 
-sudo usermod -aG docker $USER
+# Install AWS CLI
+apt install awscli -y
 
-# wait for docker
-until systemctl is-active --quiet docker; do
-  sleep 2
-done
+# Wait a little
+sleep 20
 
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 192902842773.dkr.ecr.ap-south-1.amazonaws.com
+# Login to ECR
+aws ecr get-login-password --region ap-south-1 | \
+docker login \
+--username AWS \
+--password-stdin 192902842773.dkr.ecr.ap-south-1.amazonaws.com
 
-docker rm -f frontend || true
-
+# Pull Image
 docker pull 192902842773.dkr.ecr.ap-south-1.amazonaws.com/frontend-repo:latest
 
-docker run -d --restart always \
-  --name frontend \
-  -p 8080:80 \
-  192902842773.dkr.ecr.ap-south-1.amazonaws.com/frontend-repo:latest
+# Remove old container if exists
+docker rm -f react-app || true
+
+# Run Container
+docker run -d \
+--name react-app \
+-p 80:80 \
+--restart always \
+192902842773.dkr.ecr.ap-south-1.amazonaws.com/frontend-repo:latest
+
+#update nginx config
