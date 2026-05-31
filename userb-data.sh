94fb738f48db2d5b4f21e208ab3ee9e7f32bd1ad
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-31 17:10:52 +0530

    update

--- userb-data.sh
+++ userb-data.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
+# Save logs
 exec > /var/log/user-data.log 2>&1
 
-apt update -y
-apt install docker.io awscli -y
+# Wait for system boot
+sleep 30
 
+# Update packages
+apt update -y
+
+# Install Docker + AWS CLI
+apt install -y docker.io awscli
+
+# Start Docker
 systemctl start docker
 systemctl enable docker
 
+# Add ubuntu user to docker group
+usermod -aG docker ubuntu
+
+# Wait for IAM role credentials
 sleep 20
 
+# Login to ECR
 aws ecr get-login-password --region ap-south-1 | \
 docker login \
 --username AWS \
 --password-stdin 192902842773.dkr.ecr.ap-south-1.amazonaws.com
 
+# Pull latest backend image
 docker pull 192902842773.dkr.ecr.ap-south-1.amazonaws.com/flask-backend:latest
 
+# Remove old container if exists
 docker rm -f backend || true
 
+# Run backend container
 docker run -d \
 --name backend \
 -p 5000:5000 \
